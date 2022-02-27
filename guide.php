ee875cabf723a7fab3e7bf00481751c211148831
Kalina <k.hajzner1@ncl.ac.uk>
2022-02-27 20:29:16 +0000

    Added guide page.

---
+++ guide.php
@@ -0,0 +1,17 @@
+<?php
+include "index.php";
+
+?>
+
+<html>
+<head>
+<title>Guide</title>
+</head>
+<body>
+<div class="square">
+		<p>&nbsp;&nbsp;&nbsp;Guideliness for using the website. 
+		</p>
+
+</div>
+</body>
+</html>
