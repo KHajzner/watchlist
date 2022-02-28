32ed5126563d4db2678b47a2c747ddaa5dea4f64
Kalina <k.hajzner1@ncl.ac.uk>
2022-02-28 11:55:33 +0000

    Updated guide page.

--- guide.php
+++ guide.php
@@ -8,10 +8,18 @@ include "index.php";
 <title>Guide</title>
 </head>
 <body>
+	<h1 class="title is-1">Guideliness</h1>
 <div class="square">
-		<p>&nbsp;&nbsp;&nbsp;Guideliness for using the website. 
+		<p>&nbsp;&nbsp;&nbsp;A list of guideliness for using this website. If there is anything else that is unclear or would like to be clarified,
+		you can find my contact information  <a href="main.php">
+                            here
+                        </a>
 		</p>
+		<strong style="border-bottom: 2px solid #f57aeb;">Runtime</strong>
+		<p>Represents how long a movie is in minutes.</p>
 
+		<strong style="border-bottom: 2px solid #f57aeb;">Filter Form</strong>
+		<p>Currently you can only filter movies/tv shows by one attribute at a time. </p>
 </div>
 <div id="footer">
     <footer>
