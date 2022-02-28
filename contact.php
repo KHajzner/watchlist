86a6bd69d664ed1ed6197c76257895e15c8ace05
Kalina <k.hajzner1@ncl.ac.uk>
2022-02-28 11:55:16 +0000

    Updated contact page with more relevant info.

--- contact.php
+++ contact.php
@@ -8,23 +8,46 @@ include "index.php";
 <title>Contact</title>
 </head>
 <body>
+	<h1 class="title is-1">Information</h1>
 <div class="square">
-		<p>&nbsp;&nbsp;&nbsp;This website was created by Kalina Hajzner as a small, side project.<br> If you noticed any bugs or have any ideas, or questions regarding this project
-		feel free to contact me using any of the links provided below. 
+		<h4 class="title is-1">About The Project</h4>
+		<p>&nbsp;&nbsp;&nbsp;This website was created by Kalina Hajzner as a small, side project.<br> 
+		It allows you to keeping track of all the movies/tv shows you have watched or want to watch. 
+		There are also options for movies/tvshows you are watching or have abandoned.
+
+		You can filter database based on movie's/tv show's attributes.
+<br><br>
+		Originally run using USBWebserver with externally created database.
+		<br><br>
+		<strong>Future Plans:</strong>
+		<ul>
+		<li>Potential movie database integration</li>
+<li>Adding users</li>
+<li>Filter form using multiple inputs at once</li>
+<li>Movies/TV Shows having more than one genre assigned to them</li>
+<li>Error proofing and better SQL Injefction prevention</li>
+<li>Sort by system where users can decide how the list is sorted</li>
+</ul>
 		</p>
-		<a href="https://khajzner.w3spaces.com"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+		<h4 class="title is-1">Contact</h4>
+
+		<p>&nbsp;&nbsp;&nbsp;If you want to run the program yourself or you noticed any bugs or have any ideas, or questions regarding this project
+		don't hesitate to contact me using any of the links provided below. </p>
+				<div class="contact">
+		<a href="https://khajzner.w3spaces.com"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
   <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-</svg>Portfolio (still in making)</a><br>
+</svg>&nbsp;Portfolio</a>&nbsp;&nbsp;&nbsp;
         <a href="https://github.com/KHajzner "><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-        </svg>GitHub</a><br>
+        </svg>&nbsp;GitHub</a>&nbsp;&nbsp;&nbsp;
         <a href="https://www.linkedin.com/in/kalina-hajzner-386a4222b/"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-        </svg>LinkedIn</a><br>
+        </svg>&nbsp;LinkedIn</a>&nbsp;&nbsp;&nbsp;
         <a href="mailto:khajzner@wp.pl"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
-        </svg>Email</a>
+        </svg>&nbsp;Email</a>
+</div>
 </div>
 <div id="footer">
     <footer>
